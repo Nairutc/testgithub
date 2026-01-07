0fa9591a1714da00a495d8fe0e37dded23c79a93
Nairutc <nairutc@gmail.com>
2026-01-07 13:23:30 -0400

    Agregando main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+//Variable: espacio de memoria para almacenar información importante del programa
+
+//Declaración:
+let cliente;
+
+//Asignación de valores:
+
+cliente = "Carlos";
+
+cliente = "Samuel";
+
+console.log(cliente);
+
+//Inicialización de Variables:
+
+let edad = 34;
+
+console.log(edad);
+
+
+//Constantes: Es una variable cuyo valor no puede ser modificado una vez que se asignó.
+
+const nacimiento = 1991;
+
+console.log(nacimiento);
